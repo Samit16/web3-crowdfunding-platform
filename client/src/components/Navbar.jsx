a3101fdd5aaf8f57062f2ba8920c7a038f84c085
Samit Bhisikar <samitbhisikar16@gmail.com>
2026-07-18 21:51:46 +0530

    feat: added custom campaign creation and styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
-
 import { useStateContext } from '../context';
 import { CustomButton } from './';
-import { logo, menu, search, thirdweb } from '../assets';
+import { logo, menu, search, thirdweb } from '../assets/assets';
 import { navlinks } from '../constants';
 
 const Navbar = () => {
     const navigate = useNavigate();
     const [isActive, setIsActive] = useState('dashboard');
     const [toggleDrawer, setToggleDrawer] = useState(false);
-    const { connect, address } = useStateContext();
+    // const { connect, address } = useStateContext();
 
     return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
